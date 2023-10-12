3dfd520043cc073b47a2f8e3d71f80018f09358e
khosla-a <143970960+khosla-a@users.noreply.github.com>
2023-10-11 21:38:12 -0500

    Update smtpClient.py

--- smtpClient.py
+++ smtpClient.py
@@ -45,7 +45,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     # Send DATA command and handle server response.
     # Fill in start
-    dataCommand = 'Data'
+    dataCommand = 'DATA'
     clientSocket.send(dataCommand.encode())
     recv4 = clientSocket.recv(1024).decode()
     # Fill in end
